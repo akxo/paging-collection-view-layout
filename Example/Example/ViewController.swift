1bdbe89af7c95d301f4f314a5bbd90aad308dace
ak <atrix113@aol.com>
2019-08-11 12:14:45 -0400

    adds example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Alex Kerendian on 8/11/19.
+//  Copyright © 2019 Alexander Kerendian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
