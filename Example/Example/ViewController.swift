eee5c1e620428799c72086f9ea5a58ac9b160c6f
ak <atrix113@aol.com>
2019-08-11 13:26:20 -0400

    implements horizontal paging collection view example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,12 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Constants
+    
+    let cellWidth = (3 / 4) * UIScreen.main.bounds.width
+    let sectionSpacing = (1 / 8) * UIScreen.main.bounds.width
+    let cellSpacing = (1 / 16) * UIScreen.main.bounds.width
+    
+    let colors: [UIColor] = [.red, .green, .blue, .purple, .orange, .black, .cyan]
+    let cellId = "cell id"
+    
+    // MARK: - UI Components
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = PagingCollectionViewLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: sectionSpacing, bottom: 0, right: sectionSpacing)
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        layout.minimumLineSpacing = cellSpacing
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .white
+        collectionView.decelerationRate = .fast
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        design()
+        registerCollectionViewCells()
+        applyConstraints()
     }
-
-
+    
+    // MARK: - Setup
+    
+    private func design() {
+        view.backgroundColor = .white
+    }
+    
+    private func registerCollectionViewCells() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    private func applyConstraints() {
+        view.addSubview(collectionView)
+        collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: cellWidth).isActive = true
+    }
 }
 
+// MARK: - CollectionView Data Source
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let color = colors[indexPath.item]
+        cell.backgroundColor = color
+        return cell
+    }
+}
