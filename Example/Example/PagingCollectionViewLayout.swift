b2e70920fdcb06e5b4842303a95c47e53a5474ef
ak <atrix113@aol.com>
2019-08-11 13:22:03 -0400

    removes main.storyboard and sets up initial view controller

---
+++ Example/Example/PagingCollectionViewLayout.swift
@@ -0,0 +1,9 @@
+//
+//  PagingCollectionViewLayout.swift
+//  Example
+//
+//  Created by Alex Kerendian on 8/11/19.
+//  Copyright © 2019 Alexander Kerendian. All rights reserved.
+//
+
+import Foundation
